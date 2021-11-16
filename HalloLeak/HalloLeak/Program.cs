5ee1d7004f9a558c3f1fa2e64fe7d5fffc7873ea
André <ruland@outlook.de>
2021-11-16 16:05:27 +0100

    Hallo Leak (.NET Framework)

---
+++ HalloLeak/HalloLeak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloLeak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo");
+
+            PferdeErstellen();
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+
+        private static void PferdeErstellen()
+        {
+            List<Pferd> list = new List<Pferd>();
+            for (int i = 0; /*i < 100000*/; i++)
+            {
+                var p = new Pferd() { Name = $"Pferd #{i:00000000}" };
+                list.Add(p);
+                //Console.WriteLine($"{p.Name} added");
+                //Thread.Sleep(10);
+
+            }
+        }
+    }
+
+    public class Pferd
+    {
+        public string Name { get; set; }
+    }
+}
